5fdea69109aacbb4b1494d52a36cfcb90ef80717
Lucas Arenas <lucario_9@hotmail.com>
2022-11-23 00:03:37 -0500

    POC weather forecast

---
+++ OigaDpr/OigaDpr.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OigaDpr.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            try
+            {
+                var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                    HttpMethod.Get,
+                    "registerapi",
+                    "WeatherForecast");
+
+                ViewData["WeatherForecastData"] = forecasts;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
